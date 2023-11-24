7b6c993e1f80fc1878b37a04bb84380a95b48494
franperic <fran.peric@proton.me>
2023-11-24 12:59:37 +0100

    add first tests

---
+++ tests/test_raise.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+# Get the current directory of the script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Append the parent directory to Python path
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+import numpy as np
+import pytest
+from flair.prophet_evaluator import ProphetEvaluator
+import pandas as pd
+
+
+def test_arguments():
+    with pytest.raises(TypeError):
+        ProphetEvaluator(
+            df=pd.DataFrame(
+                {
+                    "ds": pd.date_range("2005-01-01", preiods=1000, freq="M"),
+                    "y": np.random.normal(1000),
+                }
+            ),
+        )
+
+
+def test_static_ts():
+    with pytest.raises(Exception):
+        ProphetEvaluator(
+            df=pd.DataFrame(
+                {
+                    "ds": pd.date_range("2005-01-01", preiods=1000, freq="M"),
+                    "y": 54,
+                }
+            ),
+            target="y",
+            date="ds",
+            testtype="structural",
+        )
+
+
+def test_short_ts():
+    with pytest.raises(TypeError):
+        ProphetEvaluator(
+            df=pd.DataFrame(
+                {
+                    "ds": pd.date_range("2005-01-01", preiods=12, freq="M"),
+                    "y": np.random.normal(12),
+                }
+            ),
+            target="y",
+            date="ds",
+            testtype="structural",
+        )
